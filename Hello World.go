67ccf3a9f59be734b2ce36d436fe0dbb64861dc4
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-10-13 14:10:58 +0900

    ADD: Hello World

---
+++ Hello World.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// init関数は処理の初めに実行される特別な関数
+func init() {
+	fmt.Println("Init!")
+}
+
+func bazz() {
+	fmt.Println("Bazz")
+}
+
+//関数内で他の関数を使用する
+func main() {
+	bazz()
+	fmt.Println("Hello World!", "TEST TEST")
+}
