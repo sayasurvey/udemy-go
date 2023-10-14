ad0770f9fb2713170d01fb3c1564b50593a3830c
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-10-14 20:38:29 +0900

    ADD: シフトの書き方

---
+++ shift.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 << 0) // 0001 0001 → 1
+	fmt.Println(1 << 1) // 0001 0010 → 2
+	fmt.Println(1 << 2) // 0001 0100 → 4
+	fmt.Println(1 << 3) // 0001 1000 → 8
+}
