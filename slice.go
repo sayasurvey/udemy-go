3aaafc718bd6d41628564aa0d29f53cf934544b1
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-10-15 12:31:25 +0900

    ADD: mapについて

--- slice.go
+++ slice.go
@@ -26,18 +26,43 @@ func main() {
 	// n = append(n, 100, 200, 300, 400)
 	// fmt.Println(n)
 
-	n := make([]int, 3, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 0, 0)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
-	n = append(n, 1, 2, 3, 4, 5)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n := make([]int, 3, 5)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n = append(n, 0, 0)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
+	// n = append(n, 1, 2, 3, 4, 5)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(n), cap(n), n)
 
-	a := make([]int, 3)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
+	// a := make([]int, 3)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(a), cap(a), a)
 
-	b := make([]int, 0)
-	var c []int
-	fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
-	fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+	// b := make([]int, 0)
+	// var c []int
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(b), cap(b), b)
+	// fmt.Printf("len=%d cap=%d value=%v\n", len(c), cap(c), c)
+
+	m := map[string]int{"apple": 100, "banana": 200}
+	fmt.Println(m)
+	fmt.Println(m["apple"])
+	m["banana"] = 300
+	fmt.Println(m)
+	m["new"] = 500
+	fmt.Println(m)
+
+	fmt.Println(m["nothing"])
+
+	v, ok := m["apple"]
+	fmt.Println(v, ok)
+
+	v2, ok2 := m["nothing"]
+	fmt.Println(v2, ok2)
+
+	// var m3 map[string]int
+	// m3["pc"] = 5000
+	// fmt.Println(m3)
+
+	var s []int
+	if s == nil{
+		fmt.Println("Nil")
+	}
 }
