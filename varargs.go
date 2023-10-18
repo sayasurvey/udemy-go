df132f272b382cb7278c4428585196a0ad8ddec5
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-10-18 11:24:51 +0900

    ADD: 可変長引数の使い方

---
+++ varargs.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func foo(params ...int){
+	fmt.Println(len(params), params)
+	for _, param := range params{
+		fmt.Println(param)
+	}
+}
+
+func main() {
+	foo(10, 20)
+	foo(10, 20, 30)
+}
