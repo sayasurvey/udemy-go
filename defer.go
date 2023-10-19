c631c672d0a8e32151d3311a01c8ac9e582ce0f8
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-10-20 00:32:55 +0900

    ADD: deferの使い方

---
+++ defer.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func foo(){
+	defer fmt.Println("world foo")
+	fmt.Println("hello foo")
+}
+
+func main(){
+	defer fmt.Println("world")
+	foo()
+	fmt.Println("hello")
+}
